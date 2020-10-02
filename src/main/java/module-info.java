0cb06a4c131e4aa0ea0a4deb9bbc473d4481251a
ozahirnyi <ozahirny@gmail.com>
2020-10-02 21:01:37 +0300

    begin

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module tamagochi {
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires java.sql;
+    requires javafx.fxml;
+    exports world.ucode;
+}
---
+++ src/main/java/world/ucode/Main.java
@@ -0,0 +1,25 @@
+package world.ucode;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+  @Override
+  public void start(Stage stage) throws Exception {
+    Parent root = FXMLLoader.load(getClass().getResource("/testWindow.fxml"));
+
+    Scene scene = new Scene(root, 300, 275);
+
+    stage.setTitle("FXML Welcome");
+    stage.setScene(scene);
+    stage.show();
+  }
+
+  public static void main(String[] args) {
+    launch(args);
+  }
+}
